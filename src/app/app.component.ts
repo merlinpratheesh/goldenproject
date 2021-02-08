a33e22caff0d2aafcc9a284694eb8b653d42e97e
merlinpratheesh <merlinpratheesh@gmail.com>
2021-02-08 10:58:30 +0530

    Onetap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component,OnInit } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult, FirebaseuiAngularLibraryService} from 'firebaseui-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'goldenproject';
+export class AppComponent implements AfterViewInit {
+  constructor(public afAuth: AngularFireAuth, public firebaseuiAngularLibraryService: FirebaseuiAngularLibraryService) {
+    this.firebaseuiAngularLibraryService.firebaseUiInstance.disableAutoSignIn();
+}
+
+    ngAfterViewInit(): void {
+      
+  }
+  successCallback(data: FirebaseUISignInSuccessWithAuthResult) {
+    console.log('successCallback', data);
+
+  }
+
+  errorCallback(data: FirebaseUISignInFailure) {
+    console.warn('errorCallback', data);
+  }
+
+  uiShownCallback() {
+    console.log('UI shown');
+  }
+  
+  logout() {
+    this.afAuth.signOut();
+  }
 }
