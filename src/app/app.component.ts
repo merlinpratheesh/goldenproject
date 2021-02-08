96b822ac26ff26d7736eabc2077ce749d2ea5c98
merlinpratheesh <merlinpratheesh@gmail.com>
2021-02-08 11:23:34 +0530

    Onetap-Online Integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,41 @@
 import { AfterViewInit, Component,OnInit } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult, FirebaseuiAngularLibraryService} from 'firebaseui-angular';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { UserdataService } from './service/userdata.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit {
-  constructor(public afAuth: AngularFireAuth, public firebaseuiAngularLibraryService: FirebaseuiAngularLibraryService) {
-    this.firebaseuiAngularLibraryService.firebaseUiInstance.disableAutoSignIn();
-}
 
+  myonline;
+  subjectonline = new BehaviorSubject(undefined);
+  getObservableonlineSub: Subscription = new Subscription;
+
+  getObservableonline = (localonline: Observable<boolean>) => {
+    this.getObservableonlineSub?.unsubscribe();
+    this.getObservableonlineSub = localonline.subscribe((valOnline: any) => {
+      console.log(valOnline);
+      this.subjectonline.next(valOnline);
+    });
+    return this.subjectonline;
+  }
+  OnlineCheck: undefined;
+
+  constructor(public developmentservice: UserdataService, public afAuth: AngularFireAuth, public firebaseuiAngularLibraryService: FirebaseuiAngularLibraryService) 
+  
+  {
+    this.firebaseuiAngularLibraryService.firebaseUiInstance.disableAutoSignIn();
+    this.myonline = this.getObservableonline(this.developmentservice.isOnline$);
+    this.OnlineCheck = this.myonline.pipe(
+      map((onlineval: any) => {
+      return (onlineval);
+      }));
+
+  }
     ngAfterViewInit(): void {
       
   }
--- src/app/service/userdata.service.ts
+++ src/app/service/userdata.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import firebase from 'firebase/app';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { collectionData, doc } from 'rxfire/firestore';
-import { map  } from 'rxjs/operators';
+import { fromEvent, merge, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserdataService {
 
-  constructor() { }
+  isOnline$!: Observable<boolean>;
+
+
+  constructor() {
+    this.isOnline$ = merge(
+      of(null),
+      fromEvent(window, 'online'),
+      fromEvent(window, 'offline')
+    ).pipe(map(() => navigator.onLine));
+   }
 }
