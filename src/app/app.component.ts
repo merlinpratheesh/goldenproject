a9a664cc84c6649995eaa671cb8edcc67055f559
merlinpratheesh <merlinpratheesh@gmail.com>
2021-02-10 11:34:02 +0530

    edit profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CARS ,UserdataService } from './service/userdata.service';
+
+interface Car {
+  id: number;
+  profileName: string;
+  email: string;
+  gender: string;
+  areaOfinterest: string;
+  skills: string;
+  location: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {
-  title = 'goldenproject';
+
+export class AppComponent  {
+  cars: Car[] = CARS;
+  car: FormGroup;
+
+  editCar(car: Car) {
+    this.car = new FormGroup({
+      id: new FormControl(car.id),
+      profileName: new FormControl(car.profileName),
+      email: new FormControl(car.email),
+      gender: new FormControl(car.gender),
+      areaOfinterest: new FormControl(car.areaOfinterest),
+      skills: new FormControl(car.skills),
+      location: new FormControl(car.location)
+
+    });
+  }
+
+  save() {
+    let index = this.cars.findIndex(car => car.id === this.car.value.id);
+    this.cars[index] = this.car.value;
+    console.log(this.cars[index]);
+  }
+
+  cancel() {
+    this.car = null;
+  }
 }
--- src/app/service/userdata.service.ts
+++ src/app/service/userdata.service.ts
@@ -4,6 +4,22 @@ import firebase from 'firebase/app';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { collectionData, doc } from 'rxfire/firestore';
 import { map  } from 'rxjs/operators';
+
+export const CARS = [
+  {
+    id: 0,
+    profileName: 'merlinpratheesh',
+    email: 'merlinpratheesh@gmail.com',
+    gender: 'male',
+    areaOfinterest: 'Angular',
+    skills: 'Js',
+    location: 'India',
+
+
+
+
+  }
+];
 @Injectable({
   providedIn: 'root'
 })
