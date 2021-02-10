5f13326bb8c5b4f7ea5c923425bbd9cd9e3dc8f5
merlinpratheesh <merlinpratheesh@gmail.com>
2021-02-10 14:48:07 +0530

    bottom sheet worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component,Inject } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { CARS ,UserdataService } from './service/userdata.service';
+import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet';
+import {MAT_BOTTOM_SHEET_DATA} from '@angular/material/bottom-sheet';
 
-interface Car {
+interface names {
   id: number;
   profileName: string;
   email: string;
@@ -19,29 +21,95 @@ interface Car {
 })
 
 export class AppComponent  {
-  cars: Car[] = CARS;
-  car: FormGroup;
 
-  editCar(car: Car) {
-    this.car = new FormGroup({
-      id: new FormControl(car.id),
-      profileName: new FormControl(car.profileName),
-      email: new FormControl(car.email),
-      gender: new FormControl(car.gender),
-      areaOfinterest: new FormControl(car.areaOfinterest),
-      skills: new FormControl(car.skills),
-      location: new FormControl(car.location)
+  
+  constructor(private _bottomSheet: MatBottomSheet) {}
+
+  openBottomSheet(): void {
+    this._bottomSheet.open(BottomSheetOverviewExampleSheet,{data: { names: ['merlinpratheesh', 'merlinpratheesh@gmail.com','male','Angular','Js','India']} });
+  }
+
+
+}
+
+@Component({
+  selector: 'bottom-sheet-overview-example-sheet',
+  template: `
+
+    <h2 class="py-4">Edit Car</h2>
+    <form [formGroup]="names">
+      <div class="form-group row">
+        <label for="brand" class="col-sm-2 col-form-label">profileName</label>
+        <div class="col-sm-6">
+          <input type="text" class="form-control" id="brand" formControlName="profileName">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="model" class="col-sm-2 col-form-label">email</label>
+        <div class="col-sm-6">
+          <input type="text" class="form-control" id="model" formControlName="email">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="model" class="col-sm-2 col-form-label">gender</label>
+        <div class="col-sm-6">
+          <input type="text" class="form-control" id="year" formControlName="gender">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="model" class="col-sm-2 col-form-label">areaOfinterest</label>
+        <div class="col-sm-6">
+          <input type="text" class="form-control" id="year" formControlName="areaOfinterest">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="model" class="col-sm-2 col-form-label">skills</label>
+        <div class="col-sm-6">
+          <input type="text" class="form-control" id="year" formControlName="skills">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="model" class="col-sm-2 col-form-label">location</label>
+        <div class="col-sm-6">
+          <input type="text" class="form-control" id="year" formControlName="location">
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="col-sm-4 offset-sm-2">
+          <button type="submit" class="btn btn-primary mr-2" (click)="save()">Save</button>
+          <button type="reset" class="btn btn-outline-primary" (click)="cancel()">Cancel</button>
+        </div>
+      </div>
+    </form>
+  `
+})
+export class BottomSheetOverviewExampleSheet {
+
+  names: FormGroup;
+
+  constructor(private _bottomSheetRef: MatBottomSheetRef<BottomSheetOverviewExampleSheet>,@Inject(MAT_BOTTOM_SHEET_DATA) public data: {names: string[]}) {
+    console.log(this.data.names[0]);
+    this.names = new FormGroup({
+      profileName: new FormControl(this.data.names[0]),
+      email: new FormControl(this.data.names[1]),
+      gender: new FormControl(this.data.names[2]),
+      areaOfinterest: new FormControl(this.data.names[3]),
+      skills: new FormControl(this.data.names[4]),
+      location: new FormControl(this.data.names[5])
 
     });
   }
 
+
   save() {
-    let index = this.cars.findIndex(car => car.id === this.car.value.id);
-    this.cars[index] = this.car.value;
-    console.log(this.cars[index]);
+
   }
 
   cancel() {
-    this.car = null;
+    this.names = null;
+  }
+  openLink(event: MouseEvent): void {
+    this._bottomSheetRef.dismiss();
+    event.preventDefault();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppSharedModule } from './app-shared/app-shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, BottomSheetOverviewExampleSheet } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -12,7 +12,7 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,BottomSheetOverviewExampleSheet
   ],
   imports: [
     BrowserModule,
@@ -25,6 +25,7 @@ import { environment } from '../environments/environment';
     AngularFireStorageModule // storage
   ],
   providers: [],
+  entryComponents: [ BottomSheetOverviewExampleSheet],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
