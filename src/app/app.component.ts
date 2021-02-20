860a96f1b7f19d76ff2697b35c43b45517b4ff09
merlinpratheesh <merlinpratheesh@gmail.com>
2021-02-20 12:22:25 +0530

    Db Written

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,191 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component,OnInit } from '@angular/core';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult, FirebaseuiAngularLibraryService} from 'firebaseui-angular';
+import firebase from 'firebase/app';
+import '@firebase/auth';
+
+import { auth as uiAuth } from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { faMehRollingEyes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'goldenproject';
+export class AppComponent implements AfterViewInit {
+  constructor(public afAuth: AngularFireAuth, public firebaseuiAngularLibraryService: FirebaseuiAngularLibraryService,private db: AngularFirestore) {
+    this.firebaseuiAngularLibraryService.firebaseUiInstance.disableAutoSignIn();
+  }
+  ngAfterViewInit(): void {
+
+  }
+  successCallback(data: FirebaseUISignInSuccessWithAuthResult) {
+    console.log('successCallback', data);
+
+  }
+
+  errorCallback(data: FirebaseUISignInFailure) {
+    console.warn('errorCallback', data);
+  }
+
+  uiShownCallback() {
+    console.log('UI shown');
+  }
+
+  logout() {
+    this.afAuth.signOut();
+  }
+
+  SimpleFieldsNM(){
+    const data = {
+      stringExample: 'Hello, World!',
+      booleanExample: true,
+      numberExample: 3.14159265,
+      dateExample: firebase.firestore.Timestamp.fromDate(new Date('December 10, 1815')),
+      arrayExample: ['hello0', 'hello'],
+      nullExample: null,
+      objectExample: {
+        ObjarrayExample: [5, true, 'hello'],
+        projectName: 'Angular',
+      }
+    };
+    
+    const res = this.db.collection('testme').doc('one-id').set(data);
+  }
+
+  SimpleFieldsMT(){
+    const data = {
+      stringExample: 'Merge',
+      objectExample: {
+        projectName: 'React'
+      }
+    };
+    
+    const res = this.db.collection('testme').doc('one-id').set(data, {merge:true});
+  }
+  SimpleFieldsMF(){
+    const data = {
+      stringExample: 'Merge ME False',
+      objectExample: {
+        projectName: 'Angular'
+      }
+    };
+    
+    const res = this.db.collection('testme').doc('one-id').set(data, {merge:false});
+  }
+
+  MapfirestoreNM(){
+    const publicdata = {
+      stringExample: 'Hello, World!',
+      booleanExample: true,
+      numberExample: 3.14159265,
+      dateExample: firebase.firestore.Timestamp.fromDate(new Date('December 10, 1815')),
+      arrayExample: [5, true, 'hello'],
+      nullExample: null,
+      objectExample: {
+        a: 5,
+        b: true
+      }
+    };
+    
+    const res = this.db.collection('testme').doc('one-id').set({publicdata});
+  }
+
+  MapfirestoreMT(){
+    const mydata    = {
+      stringExample: 'Merge ME'
+    };
+    
+    const res = this.db.collection('testme').doc('one-id').set({mydata}, {merge:true});
+  }
+  MapfirestoreMF(){
+    const data = {
+      stringExample: 'NoMerge',
+      objectExample: {
+        b: false
+      }
+    };
+    
+    const res = this.db.collection('testme').doc('one-id').set({data}, {merge:false});
+  }
+  SArrayfirestoreNM(){
+   
+    const res = this.db.collection('testme').doc('one-id').update(
+      {arrayExample:firebase.firestore.FieldValue.arrayUnion({key: 'hello-Union'})
+      });
+  }
+
+  SArrayfirestoreMT(){
+    const res = this.db.collection('testme').doc('one-id').update(
+      {arrayExample:firebase.firestore.FieldValue.arrayRemove({key: 'hello-Union'})
+      });
+  }
+
+
+  ArrayfirestoreNM(){
+    const data = {mydata: [{
+      stringExample: 'Hello, World!',
+      booleanExample: true,
+      numberExample: 3.14159265,
+      dateExample: firebase.firestore.Timestamp.fromDate(new Date('December 10, 1815')),
+      arrayExample: [5, true, 'hello'],
+      nullExample: null,
+      objectExample: {
+        a: 5,
+        b: true
+      }
+    },
+    {
+      stringExample: 'Hello, World!',
+      booleanExample: true,
+      numberExample: 3.14159265,
+      dateExample: firebase.firestore.Timestamp.fromDate(new Date('December 10, 1815')),
+      arrayExample: [5, true, 'hello'],
+      nullExample: null,
+      objectExample: {
+        a: 5,
+        b: true
+      }
+    }
+  ]};
+    
+    const res = this.db.collection('testme').doc('one-id').set(data);
+  }
+
+  ArrayfirestoreMT(){
+    const res = this.db.collection('testme').doc('one-id').update({
+      mydata:firebase.firestore.FieldValue.arrayUnion({
+        stringExample: 'MergeMeeee',
+        booleanExample: true,
+        numberExample: 3.14159265,
+        dateExample: firebase.firestore.Timestamp.fromDate(new Date('December 10, 1815')),
+        arrayExample: [5, true, 'hello'],
+        nullExample: null,
+        objectExample: {
+          a: 5,
+          b: true
+        }
+      })
+    })
+  }
+
+  ArrayfirestoreMF(){
+    const res = this.db.collection('testme').doc('one-id').update({
+      mydata:firebase.firestore.FieldValue.arrayRemove({
+        stringExample: 'MergeMeeee',
+        booleanExample: true,
+        numberExample: 3.14159265,
+        dateExample: firebase.firestore.Timestamp.fromDate(new Date('December 10, 1815')),
+        arrayExample: [5, true, 'hello'],
+        nullExample: null,
+        objectExample: {
+          a: 5,
+          b: true
+        }
+      })
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppSharedModule } from './app-shared/app-shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppSharedModule } from './app-shared/app-shared.module';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment'
+import {firebase, firebaseui, FirebaseUIModule} from 'firebaseui-angular';
+
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInOptions: [
+      {
+        // Google provider must be enabled in Firebase Console to support one-tap
+        // sign-up.
+        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        // Required to enable ID token credentials for this provider.
+        // This can be obtained from the Credentials page of the Google APIs
+        // console. Use the same OAuth client ID used for the Google provider
+        // configured with GCIP or Firebase Auth.
+        clientId: '155833140934-om6lcebvnosmo8sgkkogpojvs74g0la5.apps.googleusercontent.com'
+      }],
+
+    credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
+
+  };
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+
     BrowserModule,
     AppRoutingModule,
-    AppSharedModule,
     BrowserAnimationsModule,
+    AppSharedModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // firestore
     AngularFireAuthModule, // auth
     AngularFireStorageModule // storage
+    ,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
